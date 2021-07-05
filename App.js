92c40ed749522f9abb621b9c6eb06870d750b96c
Alaa Abdelfattah <alaa.abdelfattah16@gmail.com>
2021-07-05 18:42:20 +0200

    Setup react-navigation

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {Text, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 const App = () => {
   return (
+    <NavigationContainer>
     <View>
       <Text>IOS,Android</Text>
     </View>
+    </NavigationContainer>
   );
 };
 
