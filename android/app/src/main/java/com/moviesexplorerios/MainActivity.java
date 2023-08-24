11cd6cc525b70ac292878eb0d06cd2c312970732
Alaa Abdelfattah <a.abdelfattah@yodawy.com>
2023-08-24 21:09:25 +0300

    link package

--- android/app/src/main/java/com/moviesexplorerios/MainActivity.java
+++ android/app/src/main/java/com/moviesexplorerios/MainActivity.java
@@ -1,6 +1,9 @@
 package com.moviesexplorerios;
 
+import android.os.Bundle; // here
 import com.facebook.react.ReactActivity;
+// react-native-splash-screen >= 0.3.1
+import org.devio.rn.splashscreen.SplashScreen; // here
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +15,9 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "MoviesExplorerIos";
   }
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
 }
