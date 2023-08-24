6f139a544370861317733d5937352a3d92399c95
Alaa Abdelfattah <a.abdelfattah@yodawy.com>
2023-08-24 21:10:08 +0300

    hide splash screen once app is loaded

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {LogBox} from 'react-native';
 import {Provider} from 'react-redux';
 import store from './src/redux/store';
 import Toast from 'react-native-toast-message';
+import SplashScreen from 'react-native-splash-screen';
 import {NavigationContainer} from '@react-navigation/native';
 
 import {ButtomTabs} from './src/Components/ButtomTabs';
@@ -11,6 +12,10 @@ LogBox.ignoreLogs(['Reanimated 2']);
 LogBox.ignoreLogs(['Encountered two children']);
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <NavigationContainer>
