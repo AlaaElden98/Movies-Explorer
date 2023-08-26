61f20b7d0828f0511dabbd160c7c8f38d1741955
Alaa Abdelfattah <a.abdelfattah@yodawy.com>
2023-08-26 19:29:12 +0300

    add icons

---
+++ src/assests/index.js
@@ -0,0 +1,10 @@
+import Tv24 from './tv24/tv24.png';
+import Film24 from './film24/film24.png';
+import List24 from './list24/list24.png';
+import Heart24 from './heart24/heart24.png';
+import Share24 from './share24/share24.png';
+import World24 from './world24/world24.png';
+import Search24 from './search24/search24.png';
+import RedHeart24 from './redHeart24/redHeart24.png';
+
+export {Tv24, Film24, Heart24, List24, Share24, World24, Search24, RedHeart24};
