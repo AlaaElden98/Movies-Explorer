10e6ea6a9096f6380ccde230abd2b00d18ef6f08
Alaa Abdelfattah <a.abdelfattah@yodawy.com>
2023-08-30 23:36:56 +0300

    update assets

--- src/assests/index.js
+++ src/assests/index.js
@@ -5,6 +5,21 @@ import Heart24 from './heart24/heart24.png';
 import Share24 from './share24/share24.png';
 import World24 from './world24/world24.png';
 import Search24 from './search24/search24.png';
+import Trending21 from './trend21/trend21.png';
 import RedHeart24 from './redHeart24/redHeart24.png';
+import MyListEmptyState from './myListEmptyState/myListEmptyState.png';
+import SearchEmptyState from './searchEmptyState/searchEmptyState.png';
 
-export {Tv24, Film24, Heart24, List24, Share24, World24, Search24, RedHeart24};
+export {
+  Tv24,
+  Film24,
+  List24,
+  Heart24,
+  Share24,
+  World24,
+  Search24,
+  RedHeart24,
+  Trending21,
+  SearchEmptyState,
+  MyListEmptyState,
+};
