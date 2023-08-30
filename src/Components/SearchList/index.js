bfcd1cfe66e082ce74f71dc52bb824ecf43667a7
Alaa Abdelfattah <a.abdelfattah@yodawy.com>
2023-08-30 23:45:03 +0300

    [SearchScreen] handle initial,loading,and no results found states

--- src/Components/SearchList/index.js
+++ src/Components/SearchList/index.js
@@ -2,14 +2,32 @@
 import React, {useEffect, useState} from 'react';
 import {useSelector} from 'react-redux';
 import LottieView from 'lottie-react-native';
-import {FlatList, View, TouchableOpacity, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 
 import {Card} from '../Card';
+import {
+  responsiveHeight,
+  responsiveFontSize,
+} from '../../Utilis/helperFunctions';
+import Colors from '../../Constants/Colors';
+import {SearchEmptyState, Trending21} from '../../assests';
 import {getSearchResults} from '../../Api/getSearchResults';
 import {getImagesBaseUrl} from '../../Api/getImagesBaseUrl';
-import {responsiveHeight} from '../../Utilis/helperFunctions';
 import AnimatedSearch from '../../assests/animatedSearch2.json';
 import {CustomActivityIndicator} from '../CustomActivityIndicator';
+
+const RenderIndex = {
+  InitialState: 0,
+  Loading: 1,
+  Results: 2,
+};
 
 export const SearchList = ({navigation}) => {
   const [page, setPage] = useState(1);
@@ -17,24 +35,28 @@ export const SearchList = ({navigation}) => {
   const [searchResults, setSearchResults] = useState([]);
   const [imageBaseUrl, setImageBaseUrl] = useState();
   const [loadMore, setLoadMore] = useState();
-  const [loading, setLoading] = useState(false);
+  const [renderIndex, setRenderIndex] = useState(RenderIndex.InitialState);
   let lottieRef = null;
 
   const query = useSelector(state => state.search.query);
+  const trendingSearches = useSelector(state => state.search.trendingSearches);
 
   const getSearch = async (pageNumber, newQuery = false) => {
-    if (query === '' || !query) {
+    if (!query) {
       setSearchResults([]);
+      setRenderIndex(RenderIndex.InitialState);
       return;
     }
-    setLoading(true);
-    if (lottieRef) lottieRef.play();
+    if (newQuery) {
+      if (lottieRef) lottieRef.play();
+      setRenderIndex(RenderIndex.Loading);
+    }
     const data = await getSearchResults(query, pageNumber);
     searchResults && !newQuery
       ? setSearchResults([...searchResults, ...data.results])
       : setSearchResults(data.results);
     setTotalPages(data.total_pages);
-    setLoading(false);
+    setRenderIndex(RenderIndex.Results);
   };
 
   useEffect(() => {
@@ -62,6 +84,14 @@ export const SearchList = ({navigation}) => {
     } else {
       setLoadMore(false);
     }
+  };
+
+  const handleOnPressTrendingItem = item => {
+    navigation.navigate('Details', {
+      parent: 'movie',
+      id: item.id,
+      imageBaseUrl: imageBaseUrl,
+    });
   };
 
   const renderItem = ({item}) => {
@@ -115,23 +145,64 @@ export const SearchList = ({navigation}) => {
     />
   );
 
-  return (
-    <View style={{flex: 1}}>
-      {loading ? (
-        renderLoading()
-      ) : (
-        <FlatList
-          data={searchResults}
-          renderItem={renderItem}
-          keyExtractor={item => item.id}
-          onEndReachedThreshold={0.5}
-          onEndReached={handleEndReached}
-          ListFooterComponent={renderFooter}
-          contentContainerStyle={{paddingBottom: 100}}
-        />
-      )}
+  const renderState = () => {
+    switch (renderIndex) {
+      case RenderIndex.InitialState:
+        return renderInitialState();
+      case RenderIndex.Results:
+        return renderResultsList();
+      case RenderIndex.Loading:
+        return renderLoading();
+      default:
+        return renderInitialState();
+    }
+  };
+
+  const renderInitialState = () => {
+    return (
+      <View style={{flex: 1}}>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={styles.initialTitle}>Trending searches</Text>
+          <Image source={Trending21} />
+        </View>
+        <View style={styles.trendingItemsContainer}>
+          {trendingSearches.map((item, index) => (
+            <TouchableOpacity
+              key={index}
+              activeOpacity={0.7}
+              style={styles.trendingItem}
+              onPress={() => handleOnPressTrendingItem(item)}>
+              <Text style={styles.trendingTitle}>{item.title}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
+    );
+  };
+
+  const renderEmptyState = () => (
+    <View style={styles.emptyStateContainer}>
+      <Image source={SearchEmptyState} />
+      <Text style={styles.emptyStateText}>
+        hmmm, we can't find the title you want!
+      </Text>
     </View>
   );
+
+  const renderResultsList = () => (
+    <FlatList
+      data={searchResults}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}
+      onEndReachedThreshold={0.5}
+      onEndReached={handleEndReached}
+      ListFooterComponent={renderFooter}
+      contentContainerStyle={{paddingBottom: 100, flex: 1}}
+      ListEmptyComponent={renderEmptyState}
+    />
+  );
+
+  return <View style={{flex: 1}}>{renderState()}</View>;
 };
 
 const styles = StyleSheet.create({
@@ -143,7 +214,32 @@ const styles = StyleSheet.create({
     overflow: 'hidden',
     marginHorizontal: 14,
     flexDirection: 'row',
-    borderColor: '#efe7e7',
+    borderColor: Colors.lightGrayishRed2,
     backgroundColor: 'white',
   },
+  trendingItem: {
+    padding: 10,
+    marginTop: 10,
+    marginRight: 10,
+    borderRadius: 20,
+    backgroundColor: Colors.lightRed,
+  },
+  trendingItemsContainer: {
+    flex: 1,
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    marginHorizontal: 12,
+  },
+  emptyStateContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  emptyStateText: {
+    paddingTop: 10,
+    color: Colors.darkGray,
+    fontSize: responsiveFontSize(1.6),
+  },
+  initialTitle: {marginHorizontal: 10, fontSize: responsiveFontSize(1.8)},
+  trendingTitle: {color: Colors.strongRed, fontSize: responsiveFontSize(1.5)},
 });
