c7154ae6414a5148927941e120c5152d371a262f
Alaa Abdelfattah <a.abdelfattah@yodawy.com>
2023-08-30 23:43:54 +0300

    update colors

--- src/Constants/Colors.js
+++ src/Constants/Colors.js
@@ -1,9 +1,12 @@
 const Colors = {
   darkGray: '#999999',
+  lightRed: '#ffcccb',
   lightGray: '#f8f8f8',
+  strongRed: '#D22B2B',
   grayishWhite: '#fafafa',
   heavyDarkGray: '#525252',
   lightGrayishRed: '#ece9e9',
+  lightGrayishRed2: '#efe7e7',
 };
 
 export default Colors;
